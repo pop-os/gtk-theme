88f5af6793ee7b8d252634ea9c7af95cb9a09f63
Cassidy James Blaede <cassidy@system76.com>
2017-12-18 15:23:46 -0700

    Move install to makefile

--- install.sh
+++ install.sh
@@ -125,13 +125,6 @@ for color in "${_COLOR_VARIANTS[@]}"; do
     cp -ur \
       assets \
       ${themedir}/gtk-common
-      
-    # Install Plank theme
-    install -d ${themedir}/plank
-    cd ${srcdir}/plank
-    cp -ur \
-      dock.theme \
-      ${themedir}/plank
 
     for version in '3.18' '3.20' '3.22'; do
       if [ "$version" == '3.18' ]; then
