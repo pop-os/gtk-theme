62e8d3703a56a223064aecdf8d284759f3ca60ad
Ian Santopietro <ian@system76.com>
2020-04-03 11:24:28 -0600

    fix: Sync with upstream theme
    This is required for release because of a new version of libsass which does not like compound selectors. It also pulls in support for a couple of minor GTK features.

--- gtk/src/light/gtk-3.0/render-assets.sh
+++ gtk/src/light/gtk-3.0/render-assets.sh
@@ -15,10 +15,9 @@ else
     echo
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
-              --export-dpi=90 \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-     && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null #\
+    # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
 fi
 if [ -f $ASSETS_DIR/$i@2.png ]; then
     echo $ASSETS_DIR/$i@2.png exists.
@@ -26,10 +25,12 @@ else
     echo
     echo Rendering $ASSETS_DIR/$i@2.png
     $INKSCAPE --export-id=$i \
-              --export-dpi=180 \
+              --export-dpi=192 \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
-     && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png
+              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null #\
+    # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
 fi
 done
+
+cp {bullet,check,dash}-symbolic.svg assets
 exit 0
